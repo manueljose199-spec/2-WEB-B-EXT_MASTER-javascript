a74957b009bab1ac21ac7527c2a62a6b0d172e6e
manueljose199-spec <manueljoseemma199@gmail.com>
2026-02-09 19:44:12 -0600

    feat: se integro la practica de campus

--- condicionales.js
+++ condicionales.js
@@ -50,3 +50,34 @@ if (esPremium || (totalCompra > 1000 && esFinDeSemana )){
 } else {
     console.log('No aplica el descuento')
 }
+
+
+//-----------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------
+
+//pracrica de campus
+
+
+let nota=prompt("ingresa el numero de la nota") ;
+nota=parseInt(nota);
+
+if (nota > 0) {
+
+    if (nota >= 90) {
+        console.log("Nota:", nota, "- Excelente");
+    } else if (nota >= 75) {
+        console.log("Nota:", nota, "- Bien");
+    } else if (nota >= 60) {
+        console.log("Nota:", nota, "- Suficiente");
+    } else {
+        console.log("Nota:", nota, "- No aprueba");
+    }
+
+} else {
+    console.log("La nota no es válida");
+}
